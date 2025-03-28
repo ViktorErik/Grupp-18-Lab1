e56f71bd84f415ddd646c6e042c890d2e5401b0f
Viktor <viktormoden@yahoo.se>
2025-03-28 11:31:14 +0100

    klar?

--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lab1/model/task_handler.dart';
+import 'package:lab1/model/types.dart';
+import 'package:lab1/pages/add_view.dart';
+import 'package:provider/provider.dart';
 import "../widgets/task_list.dart";
 
 class MainView extends StatelessWidget {
@@ -10,6 +14,7 @@ class MainView extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Things todo"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: _actions(context),
       ),
       /*
       body: Center(
@@ -22,7 +27,46 @@ class MainView extends StatelessWidget {
         ),
       ),
       */
-      body: TaskList()     
+      body: TaskList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddView(),
+              ),
+          );
+        },
+        tooltip: "Add todo",
+        child: const Icon(Icons.add),
+        ),   
+    );
+  }
+  List<Widget> _actions(context) {
+        return [_filterMenu(context)];
+  }
+  Widget _filterMenu(context) {
+    var taskHandler = Provider.of<TaskHandler>(context, listen: false);
+    return PopupMenuButton(
+      initialValue: taskHandler.mode,
+      onSelected: (FilterType item) {
+        taskHandler.setMode(item);
+      },
+    /*
+    return PopupMenuButton<FilterType>(
+      initialValue: FilterType.all,
+      onSelected: (FilterType item) {
+
+      },
+      */
+      itemBuilder: (BuildContext context) => [
+        const PopupMenuItem<FilterType>(
+          value: FilterType.all, child: Text("All")),
+        const PopupMenuItem<FilterType>(
+          value: FilterType.done, child: Text("Done")),
+        const PopupMenuItem<FilterType>(
+          value: FilterType.undone, child: Text("Undone")),
+      ],
     );
   }
 }
--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lab1/widgets/delete_button.dart';
 import 'package:lab1/widgets/status_icon.dart';
+import 'package:lab1/widgets/title_text.dart';
 import 'package:provider/provider.dart';
 import "../model/task_handler.dart";
 
@@ -9,23 +11,26 @@ class TaskList extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     var taskHandler = context.watch<TaskHandler>();
-    var tasks = taskHandler.testTasks();
+    var tasks = taskHandler.activeTasks;
 
     return ListView(children: [
-      for (final task in tasks)
-        ListTile(
-          leading: StatusIcon(task),
-          title: Text(task.title),
-          onTap: (){
-            taskHandler.toggleTask(task);
-          },
-          trailing: IconButton( 
-            icon: Icon(Icons.delete),
-            onPressed: () { 
-              taskHandler.deleteTask(task);
-            }),
-        )
-          // ListTile(title: Text(task.title)),
-    ]);
+   for (final task in tasks)
+      Card(
+        elevation: 4,
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: ListTile(
+           leading: StatusIcon(task),
+           title: TitleText(task),
+           onTap: () {
+             taskHandler.toggleTask(task);
+           },
+           trailing: DeleteButton(
+              onPressed: () {
+                 taskHandler.deleteTask(task);
+              },
+           ),
+        ),
+      ),
+ ]);
   }
 }
